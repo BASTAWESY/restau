56a94f36cc990cb20e8658c3007aec5aad10d071
unknown <mohmedel.bastawesy2020@gmail.com>
2020-11-22 01:06:30 +0200

    second try of deplor

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './users/users.module';
-
+// const MONGODB_URL = 'mongodb+srv://pasta94:pasta94@alexrestau.3cnpg.mongodb.net/<alexrestau>?retryWrites=true&w=majority'
 @Module({
   //mongodb+srv://pasta94:pasta94@alexrestau.3cnpg.mongodb.net/<dbname>?retryWrites=true&w=majority
   //mongodb://localhost/restau
-  imports: [UserModule , MongooseModule.forRoot('mongodb+srv://pasta94:pasta94@alexrestau.3cnpg.mongodb.net/<alexrestau>?retryWrites=true&w=majority')],
+  imports: [UserModule , MongooseModule.forRoot(process.env.MONGODB_URL)],
   controllers: [AppController],
   providers: [AppService],
 })
